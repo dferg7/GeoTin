d84a163d1a7dac9ccfe4f35ff764c153f6e95c6f
dferg7 <d.fergs@hotmail.com>
2015-11-03 17:23:12 -1000

    Post at current location, bounds must be within UH Manoa radius

--- app/src/main/java/com/example/ella/geotin/GeoTin.java
+++ app/src/main/java/com/example/ella/geotin/GeoTin.java
@@ -1,16 +1,22 @@
 package com.example.ella.geotin;
 
-import android.location.Address;
-import android.location.Geocoder;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
@@ -18,11 +24,12 @@ import java.util.List;
 
 public class GeoTin extends FragmentActivity {
 
-    private GoogleMap map; // Might be null if Google Play services APK is not available.
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_geo_tin);
         setUpMapIfNeeded();
     }
@@ -33,7 +40,47 @@ public class GeoTin extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
-    public void onSearch(View view) throws IOException {
+    public void onPost (View view) {
+        EditText location_tf = (EditText) findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+
+        // Get LocationManager object from system service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Get the current location
+        Location mylocation = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+
+        // Get latitude of current location
+        double latitude = mylocation.getLatitude();
+
+        // Get longitude of current location
+        double longitude = mylocation.getLongitude();
+
+        // Create latlng object of current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // show current location in google map
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in google map
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+        // Post marker only if user is within UH
+
+        if(latitude < 21.291918 && latitude > 21.310791 && longitude < -157.821747 && longitude > -157.808540)  {
+            // Add marker to current location
+            Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(location));
+
+        }
+        else
+            new AlertDialog.Builder(this).setTitle("Oh no!").setMessage("You're outside the bounds of UH Manoa :(").setNeutralButton("Okay", null).show();
+
+
+    }
+
+
+
+/*    public void onPost(View view) throws IOException {
         EditText location_tf = (EditText)findViewById(R.id.TFaddress);
         String location = location_tf.getText().toString();
         List<Address> addressList = null;
@@ -49,39 +96,46 @@ public class GeoTin extends FragmentActivity {
 
             Address address = addressList.get(0);
             LatLng latLng = new LatLng(address.getLatitude() , address.getLongitude());
-            map.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-            map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
 
         }
-    }
+    }*/
+
+
+
+
+/*    public var bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(21.291918,-157.821747),  // southwest
+            new google.maps.LatLng(21.310791,-157.808540)  // northeast
+    );*/
+
+
+
+    
 
     public void onZoom(View view) {
         if(view.getId() == R.id.Bzoomin){
-            map.animateCamera(CameraUpdateFactory.zoomIn());
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
         }
         if(view.getId() == R.id.Bzoomout){
-            map.animateCamera(CameraUpdateFactory.zoomOut());
+            mMap.animateCamera(CameraUpdateFactory.zoomOut());
         }
     }
 
     // set up map type
     public void changeType(View view) {
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-/*        if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        }
-        else
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);*/
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (map == null) {
+        if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (map != null) {
+            if (mMap != null) {
                 setUpMap();
             }
         }
@@ -89,10 +143,14 @@ public class GeoTin extends FragmentActivity {
 
 
     private void setUpMap() {
-        map.setMyLocationEnabled(true);
+        // Enable myLocation layer of google map
+        mMap.setMyLocationEnabled(true);
 
-        //map.addMarker(new MarkerOptions().position().title("I am here"));
-        //map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        
+
+        //mMap.addMarker(new MarkerOptions().position().title("I am here"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
     }
+
 }
